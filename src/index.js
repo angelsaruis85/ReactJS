78670422853a31666c4a1e2ab0e1c91d71ea71a6
angel saruis <angelsaruis@gmail.com>
2021-07-25 13:14:26 -0300

    react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<div>Curso de React</div>,
+
+
+ReactDOM.render(<React.StrictMode>
+    <App />
+  </React.StrictMode>,
     document.getElementById('root'));
